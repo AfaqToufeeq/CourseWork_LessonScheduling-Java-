6bc1560ebeaa485066c5c21d180564a79312dac7
AfaqToufeeq07 <afaqtoufeeq7@gmail.com>
2022-04-16 19:19:28 +0500

    Main File updated with days Scheduling

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 class Index {
 
+    static int sYoga=0, sRunning=0, sBodyFit=0, sBodyBlitz=0, sAquacise=0, sZumba=0;
     public Index() {
 
     }
@@ -29,8 +30,44 @@ class Price {
 
     }
 }
+
+class LessonBook{
+    void booking(){
+        Scanner o = new Scanner(System.in);
+        System.out.println("ENTER A NUMBER TO BOOK RESPECTIVE LESSON: ");
+        int bookLesson = o.nextInt();
+
+        if (bookLesson==1){
+            Index.sYoga++;
+        }
+        else if (bookLesson==2){
+            Index.sRunning++;
+
+        }
+        else if (bookLesson==3){
+            Index.sBodyFit++;
+        }
+        else if (bookLesson==4){
+            Index.sBodyBlitz++;
+
+        }
+        else if (bookLesson==5){
+            Index.sAquacise++;
+
+        }
+        else if (bookLesson==6){
+            Index.sZumba++;
+
+        }
+
+        else{
+            System.out.println("WRONG INPUT!!!");
+        }
+    }
+}
 class TimeTable{
     static String[] execrcise= new String[]{"YOGA", "RUNNING", "BODY FIT", "BODY BLITZ", "AQUACISE", "ZUMBA"};
+
     static String[] timing= new String[]{"morning", "afternoon", "evening"};
     Price p=new Price();
     Scanner o = new Scanner(System.in);
@@ -43,7 +80,7 @@ class TimeTable{
 
         while(true) {
             System.out.println("1: DAY");
-            System.out.println("2: EXERCISE NAME");
+            System.out.println("2: LESSON NAME");
             System.out.println("3: SEE PRICES");
 
             System.out.print("Choose an option: ");
@@ -94,19 +131,51 @@ class TimeTable{
 
     }
     void saturday(){
-        System.out.println("* * * * * * SATURDAY EXERCISES * * * * * *");
+        System.out.println("* * * * * * SATURDAY LESSONS * * * * * *");
 
         for (int i=0; i<3; i++){
             System.out.println("EXERCISE "+(i+1)+"--> "+ execrcise[i]+" running in "+ timing[i]);
-
         }
+
+        Scanner o= new Scanner(System.in);
+
+        System.out.println("press y yo book a lesson, and n to go to main menu: ");
+        String in = o.next();
+        if (in == "y"){
+            LessonBook b = new LessonBook();
+            b.booking();
+        }
+        else if(in == "n"){
+            selectTimeTable();
+        }
+        else{
+            System.out.println("WRONG INPUT");
+            saturday();
+        }
+
+
     }
     void sunday(){
 
-        System.out.println("* * * * * * SUNDAY EXERCISES * * * * * *");
+        System.out.println("* * * * * * SUNDAY LESSONS * * * * * *");
         for (int i=3; i<6; i++){
             System.out.print("EXERCISE "+(i+1)+"--> "+ execrcise[i]+" in "+ timing[i]);
 
+        }
+        Scanner o= new Scanner(System.in);
+
+        System.out.println("press y yo book a lesson, and n to go to main menu: ");
+        String in = o.next();
+        if (in == "y"){
+            LessonBook b = new LessonBook();
+            b.booking();
+        }
+        else if(in == "n"){
+            selectTimeTable();
+        }
+        else{
+            System.out.println("WRONG INPUT");
+            saturday();
         }
     }
 
@@ -122,7 +191,7 @@ class TimeTable{
             System.out.println((i+1)+": "+ TimeTable.execrcise[i]);
         }
         Scanner o = new Scanner(System.in);
-        System.out.print("enter number of exercise: ");
+        System.out.print("enter number of lesson: ");
         int exercise_opt= o.nextInt();
         if(exercise_opt==1 || exercise_opt==2 || exercise_opt==3){
             System.out.println("Lessons on "+ TimeTable.execrcise[exercise_opt]+ " Every Saturday in "+timing[exercise_opt]);
@@ -136,6 +205,23 @@ class TimeTable{
 
         }
 
+        Scanner opt= new Scanner(System.in);
+
+        System.out.println("press y yo book a lesson, and n to go to main menu: ");
+        String inp = o.next();
+        if (inp == "y"){
+            LessonBook b = new LessonBook();
+            b.booking();
+        }
+        else if(inp == "n"){
+            selectTimeTable();
+        }
+        else{
+            System.out.println("WRONG INPUT");
+            saturday();
+        }
+
+
 
     }
 }
@@ -144,7 +230,7 @@ class TimeTable{
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Index i = new Index();
         i.indexing();
         TimeTable t = new TimeTable();
